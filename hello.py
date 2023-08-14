122731b5126abf59ccfc68d4f2f8a805f5f0ebde
thanhNha <thanhna.k13@gmail.com>
2023-08-14 19:45:46 +0700

    kiểu dữ liệu trong python

---
+++ hello.py
@@ -0,0 +1,24 @@
+print('Hello World!')
+# Kiểu dữ liệu
+name ='Nguyễn Thanh Nhã'
+print(type(name))
+
+age = 24
+print(type(age))
+point = 8.9
+print(type(point))
+
+option = [1,2,3,4,5]
+print(type(option))
+
+isTuple = ('nguyễn Thanh nhã', 24, True)
+print(isTuple[2])
+
+dictionary = {
+    "name":'Nguyễn Thanh Nhã',
+    "age": 23,
+    'male': True,
+}
+
+
+print(type(dictionary))
